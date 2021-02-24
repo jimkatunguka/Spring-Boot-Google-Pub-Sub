7524d20cb764929a6a5394b35c6d3cba57ecb660
JXK8CIJ <jim_katunguka@homedepot.com>
2021-02-24 10:32:46 -0500

    - publish node controller end point added

--- src/main/java/com/jimkatunguka/google_pub_sub_app/GooglePubSubController.java
+++ src/main/java/com/jimkatunguka/google_pub_sub_app/GooglePubSubController.java
@@ -1,5 +1,6 @@
 package com.jimkatunguka.google_pub_sub_app;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,12 @@ public class GooglePubSubController {
 	public String publishMessage(@RequestBody MyAppGCPMessage message) {
 		messagePublisherGateway.sendToPubsub(message.toString());
 		return "Message published to Google Pub/Sub successfully";
+	}
+
+	@RequestMapping(value = "/publishNode", method = RequestMethod.POST)
+	public String publishMessage(@RequestBody JsonNode jsonNode){
+		messagePublisherGateway.sendToPubsub(jsonNode.toString());
+		return "message published to pubsub successfully";
 	}
 
 	//end point not supposed to be called. Just created to trigger the log
